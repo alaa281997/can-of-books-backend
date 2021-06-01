bb4b578203510a70003942b8f7149e50764b5c4b
alaa281997 <alaabbas97@gmail.com>
2021-06-01 23:21:10 +0300

    adding

--- server.js
+++ server.js
@@ -42,43 +42,61 @@ const myOwnerModel = mongoose.model('owner', ownerSchema);
 
 
 
-function seedOwnerCollection() {
-  const alaa = new myOwnerModel({
-    ownerEmail: 'alaabbas97@gmail.com',
-    books: [
-      {
-        bookName: 'Light',
-        description: 'One of the most underrated prose writers demonstrates the literary firepower of science fiction at its best. Three narrative strands – spanning far-future space opera, contemporary unease and virtual-reality pastiche – are braided together for a breathtaking metaphysical voyage in pursuit of the mystery at the heart of reality.',
-        urlImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJwwPWh_fnncXcnbYePfsLUTXEPpcVH8i0_A&usqp=CAU'
-      },
-      {
-        bookName: 'Light1',
-        description: '1One of the most underrated prose writers demonstrates the literary firepower of science fiction at its best. Three narrative strands – spanning far-future space opera, contemporary unease and virtual-reality pastiche – are braided together for a breathtaking metaphysical voyage in pursuit of the mystery at the heart of reality.',
-        urlImg: '2https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJwwPWh_fnncXcnbYePfsLUTXEPpcVH8i0_A&usqp=CAU'
-      }
-    ]
-  })
+function seedBookCollection() {
+    const Tipping1 = new bookModel({
+        bookName: 'The Tipping Point',
+        description: 'The New Yorker staff writer examines phenomena from shoe sales to crime rates through the lens of epidemiology, reaching his own tipping point, when he became a rock-star intellectual and unleashed a wave of quirky studies of contemporary society. Two decades on, Gladwell is often accused of oversimplification and cherry picking, but his idiosyncratic bestsellers have helped shape 21st-century culture.',
+        urlImg: 'https://images-na.ssl-images-amazon.com/images/I/61peRlJRMLL.jpg'
+    });
+    const Darkmans = new bookModel({
+        bookName: 'Darkmans',
+        description: 'British fiction’s most anarchic author is as prolific as she is playful, but this freewheeling, visionary epic set around the Thames Gateway is her magnum opus. Barker brings her customary linguistic invention and wild humour to a tale about history’s hold on the present, as contemporary Ashford is haunted by the spirit of a medieval jester.',
+        urlImg: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1432299845l/25581537._SY475_.jpg'
+    });
+    
+    Tipping1.save();
+    Darkmans.save();
+}
+//seedBookCollection();
 
-  alaa.save();
+function seedOwnerCollection() {
+    const alaa = new myOwnerModel({
+        ownerEmail: 'alaabbas97@gmail.com',
+        books: [
+            {
+              bookName: 'The Tipping Point',
+              description: 'The New Yorker staff writer examines phenomena from shoe sales to crime rates through the lens of epidemiology, reaching his own tipping point, when he became a rock-star intellectual and unleashed a wave of quirky studies of contemporary society. Two decades on, Gladwell is often accused of oversimplification and cherry picking, but his idiosyncratic bestsellers have helped shape 21st-century culture.',
+              urlImg: 'https://images-na.ssl-images-amazon.com/images/I/61peRlJRMLL.jpg'
+            },
+            {
+              bookName: 'Darkmans',
+              description: 'British fiction’s most anarchic author is as prolific as she is playful, but this freewheeling, visionary epic set around the Thames Gateway is her magnum opus. Barker brings her customary linguistic invention and wild humour to a tale about history’s hold on the present, as contemporary Ashford is haunted by the spirit of a medieval jester.',
+              urlImg: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1432299845l/25581537._SY475_.jpg'
+            }
+        ]
+    })
+
+    alaa.save();
 }
 
 
+//seedOwnerCollection();
 
 
  app.get('/books',getBooksHandler);
 
- function getBooksHandler(req,res) {
-  const email = req.query.email;
+ function getBooksHandler(req, res) {
+  let { email } = req.query;
   // let {name} = req.query
-  myOwnerModel.find({email:email},function(err,ownerData){
-      if(err) {
-          console.log('did not work')
-      } else {
-          console.log(ownerData)
-          // console.log(ownerData[0])
-          // console.log(ownerData[0].books)
-          res.send(ownerData[0].books)
-      }
+  myOwnerModel.find({ ownerEmail: email }, function (err, ownerData) {
+    if (err) {
+      console.log('did not work')
+    } else {
+      console.log(ownerData)
+      // console.log(ownerData[0])
+      // console.log(ownerData[0].books)
+      res.send(ownerData[0].books)
+    }
   })
 }
 
@@ -91,3 +109,4 @@ app.get('/test', (request, response) => {
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
